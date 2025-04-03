f6a7d5bd5829601f50a9def59aaac72e8c0f73a9
Félix MARQUET <72651575+BreizhHardware@users.noreply.github.com>
2025-04-03 16:27:16 +0200

    Better callback handling

--- TD-1-test/Core/Src/stm32l1xx_it.c
+++ TD-1-test/Core/Src/stm32l1xx_it.c
@@ -205,8 +205,6 @@ void EXTI15_10_IRQHandler(void)
 {
   /* USER CODE BEGIN EXTI15_10_IRQn 0 */
 
-  *(volatile uint32_t *)0x40020014 ^= (1 << 5);
-
   /* USER CODE END EXTI15_10_IRQn 0 */
   HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
 
